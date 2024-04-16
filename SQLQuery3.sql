041150e00ee7ecd38f968369e290cfb57ce4262a
Vitaliy Hreva <vitaliyhreva@gmail.com>
2024-04-16 23:21:08 +0300

    Update SQLQuery3.sql

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -1,19 +1,20 @@
---Визначаєм кількість замовлень кожного клієнта
+--Р’РёР·РЅР°С‡Р°С”Рј РєС–Р»СЊРєС–СЃС‚СЊ Р·Р°РјРѕРІР»РµРЅСЊ РєРѕР¶РЅРѕРіРѕ РєР»С–С”РЅС‚Р°
 SELECT c.FirstName + ' ' + c.LastName AS CustomerName, COUNT(o.OrderID) AS OrderCount FROM Customers c
 JOIN Orders o ON c.CustomerID = o.CustomerID
 GROUP BY c.CustomerID, c.FirstName, c.LastName
 
---Визначаєм середню вартість кожного замовлення
+--Р’РёР·РЅР°С‡Р°С”Рј СЃРµСЂРµРґРЅСЋ РІР°СЂС‚С–СЃС‚СЊ РєРѕР¶РЅРѕРіРѕ Р·Р°РјРѕРІР»РµРЅРЅСЏ
 SELECT AVG(TotalAmount) AS AverageOrderCost
 FROM orders;
 
---Визначаєм найбільш популярний товар
+--РЁСѓРєР°С”Рј РЅР°Р№РїРѕРїСѓР»СЏСЂРЅС–С€РёР№ С‚РѕРІР°СЂ
 SELECT TOP 1 p.ProductName, SUM(od.Quantity) AS TotalSold FROM Products p
 JOIN order_details od ON od.productID =  p.ProductID
 GROUP BY p.ProductName
 ORDER BY TotalSold DESC
 
---Кількість замовлень за кожний місяць
+
+--РљС–Р»СЊРєС–СЃС‚СЊ Р·Р°РјРѕРІР»РµРЅСЊ Р·Р° РєРѕР¶РЅРёР№ РјС–СЃСЏС†СЊ
 SELECT YEAR(OrderDate) AS OrderYear, 
 	   MONTH(OrderDate) AS OrderMonth, 
 	   COUNT(*) AS OrderCount
@@ -21,7 +22,8 @@ FROM orders
 GROUP BY YEAR(OrderDate), MONTH(OrderDate)
 ORDER BY OrderYear, OrderMonth;
 
---Кількість замовлень за кожний місяць
+
+--РЎРµСЂРµРґРЅСЏ С†С–РЅР° Р·Р°РјРѕРІР»РµРЅСЊ Р·Р° РєРѕР¶РЅРёР№ РјС–СЃСЏС†СЊ
 SELECT YEAR(OrderDate) AS OrderYear, 
 	   MONTH(OrderDate) AS OrderMonth, 
 	   AVG(TotalAmount) AS AverageSum
@@ -29,7 +31,7 @@ FROM orders
 GROUP BY YEAR(OrderDate), MONTH(OrderDate)
 ORDER BY OrderYear, OrderMonth DESC
 
---Загальна вартість замовлень за кожний місяць
+--Р—Р°РіР°Р»СЊРЅР° РІР°СЂС‚С–СЃС‚СЊ Р·Р°РјРѕРІР»РµРЅСЊ Р·Р° РєРѕР¶РЅРёР№ РјС–СЃСЏС†СЊ
 SELECT YEAR(OrderDate) AS OrderYear, 
 	   MONTH(OrderDate) AS OrderMonth,
 	   SUM(TotalAmount) AS TotalSum
@@ -37,20 +39,19 @@ FROM orders
 GROUP BY YEAR(OrderDate), MONTH(OrderDate)
 ORDER BY OrderYear, OrderMonth DESC
 
---Середній чек кожного клієнта
-SELECT * FROM orders
+--РЎРµСЂРµРґРЅС–Р№ С‡РµРє РєРѕР¶РЅРѕРіРѕ РєР»С–С”РЅС‚Р°
 SELECT c.CustomerID, c.FirstName + ' ' + c.LastName AS CustomerName,  AVG(o.TotalAmount) AS AverageCheck FROM customers c 
 JOIN Orders o ON o.CustomerID = c.CustomerID
 GROUP BY c.CustomerID , c.FirstName, c.LastName
 
---Кількість замовлень за кожний день тижня
+--РљС–Р»СЊРєС–СЃС‚СЊ Р·Р°РјРѕРІР»РµРЅСЊ Р·Р° РєРѕР¶РЅРёР№ РґРµРЅСЊ С‚РёР¶РЅСЏ
 SELECT DATEPART(WEEKDAY, OrderDate) AS Weekday,
        COUNT(*) AS OrderCount
 FROM orders
 GROUP BY DATEPART(WEEKDAY, OrderDate)
 ORDER BY Weekday;
 
---Загальна вартість замовлень кожного покупця
+--Р—Р°РіР°Р»СЊРЅР° РІР°СЂС‚С–СЃС‚СЊ Р·Р°РјРѕРІР»РµРЅСЊ РєРѕР¶РЅРѕРіРѕ РїРѕРєСѓРїС†СЏ
 SELECT c.FirstName, c.LastName, SUM(o.TotalAmount) AS TotalOrderAmount FROM Customers c
 JOIN Orders o  ON c.CustomerID = o.CustomerID
 GROUP BY c.CustomerID, c.FirstName, c.LastName
